05cea54c38fcdea843f9f5afc271a3c8b9ecfc8c
Vladimir Skirga <vladimir.skirga84@gmail.com>
2018-12-11 13:45:40 +0200

    Create scroll plugin

--- js/main.js
+++ js/main.js
@@ -1,67 +1,62 @@
- MySlider = function () {
-    var $slider;
-    var current = 0;
-    var total;
-    var bulletContainer;
+(function($){
+    jQuery.fn.mySlider = function(){
+        var make = function(){
+            var slider = $(this);
+            var items = slider.find('.slider-item');
+            var current = 0;
+            var total = items.length;
+            var bulletContainer = slider.find('.slider-bullets');
 
-    var initSlider = function (e) {
-        $slider = $('#' + e);
-        items = $slider.find('.slider-item');
-        total = items.length;
-        $slider.find('.to-left a').bind('click', toLeft);
-        $slider.find('.to-right a').bind('click', toRight);
-        bulletContainer = $slider.find('.slider-bullets');
-
-        items.each(function(index) {
-            if (index != 0) {
-                $(this).hide();
-                bulletContainer.append('<a href="javascript:;" data-item="' + (index) + '"></a>');
-            } else {
-                bulletContainer.append('<a href="javascript:;" data-item="' + (index) + '" class="slider-bullet-fill"></a>');
+            var toLeft = function () {
+                changeSlide(current - 1);
             }
-        });
 
-        bulletContainer.find('a').bind('click', function () {
-            index = parseInt($(this).data('item'));
-
-            if (index != current) {
-                changeSlide(index);
+            var toRight = function () {
+                changeSlide(current + 1);
             }
-        });
-    }
 
-    var toLeft = function () {
-        changeSlide(current - 1);
-    }
+            var changeSlide = function (index) {
+                if (index < 0) {
+                    index = total - 1;
+                } else if (index == total) {
+                    index = 0;
+                }
 
-    var toRight = function () {
-        changeSlide(current + 1);
-    }
+                slider.find('.slider-item:nth-child(' + (current + 1) + ')').fadeOut( "slow", function() {
+                    bulletContainer.find('a').removeClass('slider-bullet-fill');
+                    bulletContainer.find('a:nth-child(' + (index + 1) + ')').addClass('slider-bullet-fill');
+                    slider.find('.slider-item:nth-child(' + (index + 1) + ')').fadeIn('slow');
+                });
 
-    var changeSlide = function (index) {
-        if (index < 0) {
-            index = total - 1;
-        } else if (index == total) {
-            index = 0;
-        }
+                current = index;
+            }
 
-        $slider.find('.slider-item:nth-child(' + (current + 1) + ')').fadeOut( "slow", function() {
-            bulletContainer.find('a').removeClass('slider-bullet-fill');
-            bulletContainer.find('a:nth-child(' + (index + 1) + ')').addClass('slider-bullet-fill');
-            $slider.find('.slider-item:nth-child(' + (index + 1) + ')').fadeIn('slow');
-        });
+            slider.find('.to-left a').bind('click', toLeft);
+            slider.find('.to-right a').bind('click', toRight);
 
-        current = index;
-    }
+            items.each(function(index) {
+                if (index != 0) {
+                    $(this).hide();
+                    bulletContainer.append('<a href="javascript:;" data-item="' + (index) + '"></a>');
+                } else {
+                    bulletContainer.append('<a href="javascript:;" data-item="' + (index) + '" class="slider-bullet-fill"></a>');
+                }
+            });
 
-    return {
-        init: function (e) {
-            initSlider(e);
-        }
+            bulletContainer.find('a').bind('click', function () {
+                index = parseInt($(this).data('item'));
+
+                if (index != current) {
+                    changeSlide(index);
+                }
+            });
+        };
+
+        return this.each(make);
     };
-}();
+})(jQuery);
 
 $(document).ready(function () {
-    MySlider.init('testimonials-slider');
-    MySlider.init('videos-slider');
+    $('#testimonials-slider').mySlider();
+    $('#videos-slider').mySlider();
 });
